5bfbf95821513091327cc2366b328bcb821dbc6d
Eden Pardo <155780019+edenlpardo@users.noreply.github.com>
2025-08-06 11:03:12 -0700

    utils file

---
+++ frontend/src/utils/formatDate.js
@@ -0,0 +1,5 @@
+export function formatDate(dateStr) {
+  if (!dateStr) return '';
+  const date = new Date(dateStr);
+  return date.toLocaleDateString('en-US', { month: 'short', day: '2-digit', year: 'numeric' });
+}
